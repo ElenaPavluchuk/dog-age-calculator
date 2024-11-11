a1d0776ec2625a69243dcc999aaa453d58fd1f48
Елена Павлючук <elenapavlucuk@MacBook-Air-Elena.local>
2024-11-12 01:52:20 +0300

    console.log remove

--- index.js
+++ index.js
@@ -11,20 +11,4 @@ function handleClick() {
     const result = document.getElementById("result");
     result.classList.remove("hidden");
     result.innerText = `Dog age is: ${dogYears}`;
-    console.log(dogYears);
 };
-
-// const calculateDogAge = () => {
-//     const humanAgeInput = document.getElementById("human-age");
-//     const humanYears = parseInt(humanAgeInput.value, 10);
-
-//     if (isNaN(humanYears) || humanYears < 0) {
-//         alert("Please enter a valid non-negative number for human age.");
-//         return;
-//     }
-
-//     const dogYears = humanYears * 7 + 3;
-//     const result = document.getElementById("result");
-//     result.innerText = `Dog age is ${dogYears}`;
-//     result.classList.remove("hidden");
-// };
